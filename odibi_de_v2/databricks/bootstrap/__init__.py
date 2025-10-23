30cd4f4f17e602923e660a3f80e1ec38502aa9e7
Henry Odibi <71342766+henryodibi11@users.noreply.github.com>
2025-10-23 14:48:41 -0500

    Add init_local_connection to module exports

--- odibi_de_v2/databricks/bootstrap/__init__.py
+++ odibi_de_v2/databricks/bootstrap/__init__.py
@@ -2,11 +2,13 @@ from .init_spark_with_azure_secrets import init_spark_with_azure_secrets
 from .init_sql_config_connection import init_sql_config_connection
 from .config_loader import load_ingestion_config_tables
 from .connection_factory import BuildConnectionFromConfig
+from .init_local_connection import init_local_connection
 
 
 __all__=[
     "init_spark_with_azure_secrets",
     "init_sql_config_connection",
     "load_ingestion_config_tables",
-    "BuildConnectionFromConfig"
+    "BuildConnectionFromConfig",
+    "init_local_connection"
 ]
