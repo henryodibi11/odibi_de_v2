fd55e6dceae3dfc09aadb1e180923a9eddc01871
Henry Odibi <71342766+henryodibi11@users.noreply.github.com>
2025-10-24 11:39:17 -0500

    Rename ingestion config loader import

--- odibi_de_v2/databricks/bootstrap/__init__.py
+++ odibi_de_v2/databricks/bootstrap/__init__.py
@@ -1,6 +1,6 @@
 from .init_spark_with_azure_secrets import init_spark_with_azure_secrets
 from .init_sql_config_connection import init_sql_config_connection
-from .config_loader import load_ingestion_config_tables
+from .config_loader import load_ingestion_configs_from_sql
 from .connection_factory import BuildConnectionFromConfig
 from .local_initializer import init_local_connection
 
@@ -8,7 +8,7 @@ from .local_initializer import init_local_connection
 __all__=[
     "init_spark_with_azure_secrets",
     "init_sql_config_connection",
-    "load_ingestion_config_tables",
+    "load_ingestion_configs_from_sql",
     "BuildConnectionFromConfig",
     "init_local_connection"
 ]
